4845958ae06dcde29d2282d0ff090e41f06a18ec
Sogrub <92834529+Sogrub@users.noreply.github.com>
2022-03-13 12:01:10 -0500

    Start Project

---
+++ src/students.java
@@ -0,0 +1,15 @@
+public class students {
+    //Atributos
+    int id;
+    String fullName;
+    int age;
+    String address;
+    int phone;
+    int yearSchool;
+    float pensionValue;
+
+    //Constructor
+    public students(){
+        
+    }
+}
