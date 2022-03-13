c10d3dcd864796bac9834fd7d929538ca8ad3633
Sogrub <92834529+Sogrub@users.noreply.github.com>
2022-03-13 13:46:02 -0500

    First changes v0.1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Scanner sc = new Scanner(System.in);
+        students student1 = new students(0, "Carlos Alonso", 14, "Calle 23 5 42", "3112063020", 2022, 300000);
+        students student2 = new students(1, "Daniela Lopez", 13, "Calle 5 17 30", "3104225025", 2022, 300000);
+        students student3 = new students(2, "Sofia Lara", 14, "Calle 50 2 20", "3125429746", 2022, 300000);
+        students[] arrayStudents = {student1, student2, student3};
+        System.out.println("Bienvenidos al registro de Estudiantes y administrativos: \n");
+        for (int i = 0; i < arrayStudents.length; i++) {
+            System.out.println("los datos son: \n" + 
+            "Id: " + arrayStudents[i].id + "\n" + 
+            "Nombre: " + arrayStudents[i].fullName + "\n" + 
+            "Edad: " + arrayStudents[i].age + "\n" + 
+            "Dirección: " + arrayStudents[i].address + "\n" + 
+            "Telefono: " + arrayStudents[i].phone + "\n" + 
+            "Año escolar: " + arrayStudents[i].yearSchool + "\n" + 
+            "Valor Pension: " + arrayStudents[i].pensionValue);
+        }
+        /*System.out.println("Marca 1 si es estudiante o 2 si es administrativo:");
+        int n = sc.nextInt();
+        if (n == 1) {
+            System.out.println("Ingresaras a un Estudiante");
+        } else if (n == 2) {
+            System.out.println("Ingresaras a un Administrativo");
+        } else {
+            System.out.println("El número ingresado no es correcto");
+        }*/
+
     }
 }
--- src/students.java
+++ src/students.java
@@ -1,15 +1,32 @@
+import java.util.Scanner;
+
 public class students {
+    Scanner sc = new Scanner(System.in);
+
     //Atributos
     int id;
     String fullName;
     int age;
     String address;
-    int phone;
+    String phone;
     int yearSchool;
     float pensionValue;
 
     //Constructor
-    public students(){
+    public students(int ID, String Name, int age_, String address_, String phone_, int yearSchool_, float pensionValue_){
+        this.id = ID;
+        this.fullName = Name;
+        this.age = age_;
+        this.address = address_;
+        this.phone = phone_;
+        this.yearSchool = yearSchool_;
+        this.pensionValue = pensionValue_;
+    }
+
+    //Metodos
+
+    //Tiene que devolver los atributos como una cadena de texto
+    public void imprimir(){
         
     }
 }
